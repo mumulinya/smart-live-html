dc43da510a39e491a94cb852512d3472a01daf69
木木林 <2065942553@qq.com>
2025-10-07 12:07:09 +0800

    修改用户个人信息页面

--- js/footer.js
+++ js/footer.js
@@ -1,30 +1,40 @@
+// 底部导航栏组件
 Vue.component("footBar", {
   template: `
-    <div class="foot">
-    <div class="foot-box" :class="{active: activeBtn === 1}" @click="toPage(1)">
-      <div class="foot-view"><i class="el-icon-s-home"></i></div>
-      <div class="foot-text">首页</div>
+    <div class="foot-bar">
+      <div class="foot-item" :class="{active: activeBtn === 1}" @click="toPage(1)">
+        <div class="foot-icon">
+          <i class="el-icon-s-home"></i>
+        </div>
+        <div class="foot-text">首页</div>
+      </div>
+      <div class="foot-item" :class="{active: activeBtn === 2}" @click="toPage(2)">
+        <div class="foot-icon">
+          <i class="el-icon-map-location"></i>
+        </div>
+        <div class="foot-text">地图</div>
+      </div>
+      <div class="foot-center" @click="toPage(0)">
+        <div class="add-btn">
+          <i class="el-icon-plus"></i>
+        </div>
+      </div>
+      <div class="foot-item" :class="{active: activeBtn === 3}" @click="toPage(3)">
+        <div class="foot-icon">
+          <i class="el-icon-chat-dot-round"></i>
+        </div>
+        <div class="foot-text">消息</div>
+      </div>
+      <div class="foot-item" :class="{active: activeBtn === 4}" @click="toPage(4)">
+        <div class="foot-icon">
+          <i class="el-icon-user"></i>
+        </div>
+        <div class="foot-text">我的</div>
+      </div>
     </div>
-    <div class="foot-box" :class="{active: activeBtn === 2}" @click="toPage(2)">
-      <div class="foot-view"><i class="el-icon-map-location" @></i></div>
-      <div class="foot-text" >地图</div>
-    </div>
-    <div class="foot-box" @click="toPage(0)">
-      <img class="add-btn" src="/imgs/add.png" alt="">
-    </div>
-    <div class="foot-box" :class="{active: activeBtn === 3}" @click="toPage(3)">
-      <div class="foot-view"><i class="el-icon-chat-dot-round"></i></div>
-      <div class="foot-text">消息</div>
-    </div>
-    <div class="foot-box" :class="{active: activeBtn === 4}" @click="toPage(4)">
-      <div class="foot-view"><i class="el-icon-user"></i></div>
-      <div class="foot-text">我的</div>
-    </div>
-  </div>
   `,
   data() {
-    return {
-    }
+    return {}
   },
   props: ['activeBtn'],
   methods: {
